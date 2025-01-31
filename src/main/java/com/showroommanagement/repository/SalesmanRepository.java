c95ba9369888e250fe66ef299d8830e43af45795
Giriseelan-Jayaprakash <girijayaprakash08@gmail.com>
2025-01-31 19:31:48 +0530

    first commit1

--- src/main/java/com/showroommanagement/repository/SalesmanRepository.java
+++
@@ -1,9 +0,0 @@
-package com.showroommanagement.repository;
-
-import com.showroommanagement.entity.Salesman;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SalesmanRepository extends JpaRepository<Salesman, Integer> {
-}
